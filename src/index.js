ccaf8b717d798f6f63fbef6659a207a37bfc898c
lazarowend <lazaroclans1@gmail.com>
2024-01-08 17:21:21 -0300

    create account

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid')
+
 const app = express();
 
+app.use(express.json());
 console.log('server start');
+
+const customers = [];
+
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body;
+
+    const customerAlreadyExists = customers.some((customer) => customer.cpf == cpf)
+
+    if (customerAlreadyExists) {
+        return response.status(400).json({ error: "Customer already exists!" })
+    }
+
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+    });
+
+    return response.status(201).send()
+})
 
 app.listen(7000);
