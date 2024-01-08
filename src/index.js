2a6bc6017686b34abfa46fe209b64a9d12dd278f
lazarowend <lazaroclans1@gmail.com>
2024-01-08 17:25:40 -0300

    search statement

--- src/index.js
+++ src/index.js
@@ -26,5 +26,17 @@ app.post("/account", (request, response) => {
 
     return response.status(201).send()
 })
+ 
+app.get("/statement/:cpf", (request, response) => {
+    const { cpf } = request.params;
+    const customer = customers.find((customer) => customer.cpf == cpf);
+
+    if(!customer) {
+        return response.status(400).json({ error: "Costumer not found" })
+    }
+
+    return response.json(customer.statement)
+})
+
 
 app.listen(7000);
